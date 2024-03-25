9e2f0929157b51b66f021423cbb033537d5be0ea
josnif <josephnifemi89@gmail.com>
2024-03-25 15:12:33 +0100

    first commit

---
+++ custom-plugin-for-wp-headless-settings.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Custom Plugin for WP Headless Settings
+Description: Clean Admin panel and other headless settings
+Author: Jotweb
+*/
+
+function remove_menus(){
+
+    // remove_menu_page( 'index.php' );                  //Dashboard  
+    // remove_menu_page( 'edit.php' );                   //Posts  
+    remove_menu_page( 'edit.php?post_type=page' );    //Pages  
+    remove_menu_page( 'edit-comments.php' );          //Comments  
+    remove_menu_page( 'themes.php' );                 //Appearance  
+    // remove_menu_page( 'plugins.php' );                //Plugins  
+    // remove_menu_page( 'users.php' );                  //Users  
+    // remove_menu_page( 'tools.php' );                  //Tools  
+    // remove_menu_page( 'options-general.php' );        //Settings
+    // remove_menu_page( 'wpcf7' );        //contact form
+  
+}
+
+add_action( 'admin_menu', 'remove_menus' );
+
